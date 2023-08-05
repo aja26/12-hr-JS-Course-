de2a6d19779cf48f13ffcf1b501e32585507b453
Adam Alexander <46898882+aja26@users.noreply.github.com>
2023-08-05 21:45:33 +0100

    Add files via upload

---
+++ pract-3 strings/app.js
@@ -0,0 +1,40 @@
+const name = 'Adam';
+const age = 34;
+
+x = `hello my name is ${name} and I am ${age} years old`;
+// console.log(x);
+
+function sayHi(names){
+    // return `${s} ${name}`.toUpperCase();
+    // return `${s} ${name}`.indexOf('s');
+    // return `${s} ${name}`.substring(8, 12);
+    // return `${s} ${name}`.slice(2, 4);
+    // return `${s} ${name}`.trim();
+    // return `${ss} ${name}`;
+    // return `${s2}`; 
+    return `${s4}`; 
+}
+
+const s   = '     hello';
+const ss  = s.replace('hello', 'Greetings');
+const s2  = s.includes('hell');
+const s3  = name.valueOf();
+const s4  = name.split();
+
+
+// console.log(sayHi('bob').length);
+
+// console.log(sayHi(name));
+
+const newName = 'Barbara Smith';
+
+//console.log(newName.split(''));
+
+
+const myString = 'developer';
+const cap = myString[0].toUpperCase();
+
+const word = myString.substring(1);
+const myNewString = myString[0].toUpperCase() + myString.substring(1);
+
+console.log(myNewString);
