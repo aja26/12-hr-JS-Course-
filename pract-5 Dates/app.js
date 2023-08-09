a2ff81b620885bce81540daca0b48488fdd25254
Adam Alexander <46898882+aja26@users.noreply.github.com>
2023-08-09 14:58:23 +0100

    Add files via upload

---
+++ pract-5 Dates/app.js
@@ -0,0 +1,17 @@
+let d;
+
+d = new Date();
+
+d = new Date(2021, 0, 10, 12, 30, 0);
+d = new Date('2021-07-10T05:30:00');
+d = new Date('07/10/2021 11:30:00');
+
+d = Date.now();
+
+d = new Date('07-10-2022 12:30:00');
+d = d.getTime();
+
+d = new Date(1657452600000);
+
+console.log(d);
+
