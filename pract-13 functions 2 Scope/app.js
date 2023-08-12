f2e080992006491abf6c96deb7cb4f6e70f14b5b
Adam Alexander <46898882+aja26@users.noreply.github.com>
2023-08-12 23:18:44 +0100

    Add files via upload

---
+++ pract-13 functions 2 Scope/app.js
@@ -0,0 +1,24 @@
+const a = 100;
+const anus = 'lols';
+var anuses = 'ais' ;
+
+// for(let i = 0; i < 10; i++ ){
+//     console.log(i);
+// }
+
+if(true){
+    const a = 100;
+    let b = 200;
+    var c = 100;
+
+}
+
+console.log(c);
+
+function doSomething(){
+    var p = 666;
+    console.log(p);
+}
+
+doSomething();
+
