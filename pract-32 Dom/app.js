cac67a5993ae746afb11998ccfa741f12ff371e9
Adam Alexander <46898882+aja26@users.noreply.github.com>
2023-08-20 23:13:36 +0100

    Add files via upload

---
+++ pract-32 Dom/app.js
@@ -0,0 +1,14 @@
+const main = document.getElementById('main');
+
+
+main.innerHTML = `
+<h1>testing copy</h1>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio modi aut 
+sapiente natus sunt incidunt, repellendus alias. Numquam neque unde rem consequatur 
+eaque expedita modi similique, magni doloremque dolorum, quisquam incidunt, 
+delectus soluta minima odit inventore tenetur facilis iure! Tempora.</p>
+
+`;
+
+const para = document.querySelector('p');
+para.style.backgroundColor = 'red';
